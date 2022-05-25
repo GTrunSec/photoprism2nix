7e4756f1ede010476fdf459f3ae7121b5a28b1ed
GTrunSec <gtrunsec@hardenedlinux.org>
2022-05-24 18:58:48 -0700

    Update

--- flake.nix
+++ flake.nix
@@ -1,8 +1,8 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/release-21.11";
-    ranz2nix = {
-      url = "github:andir/ranz2nix";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    npmlock2nix = {
+      url = "github:nix-community/npmlock2nix";
       flake = false;
     };
     photoprism = {
@@ -23,11 +23,10 @@
   outputs = inputs @ {
     self,
     nixpkgs,
-    ranz2nix,
     photoprism,
     flake-utils,
     gomod2nix,
-    flake-compat,
+    ...
   }:
     flake-utils.lib.eachSystem ["x86_64-linux" "x86_64-darwin" "i686-linux"]
     (
@@ -147,14 +146,12 @@
 
             systemd.services.photoprism = {
               enable = true;
-              after = [
-                "network-online.target"
-                (
-                  if cfg.mysql
-                  then "mysql.service"
-                  else ""
-                )
-              ];
+              after =
+                [
+                  "network-online.target"
+                ]
+                ++ lib.optional cfg.mysql "mysql.service";
+
               wantedBy = ["multi-user.target"];
 
               confinement = {
@@ -195,7 +192,7 @@
                 CacheDirectory = "photoprism";
                 StateDirectory = "photoprism";
                 SyslogIdentifier = "photoprism";
-                #Sops secrets PHOTOPRISM_ADMIN_PASSWORD= /****/
+                # Sops secrets PHOTOPRISM_ADMIN_PASSWORD= /****/
                 PrivateTmp = true;
                 PrivateUsers = true;
                 PrivateDevices = true;
@@ -203,7 +200,7 @@
                 ProtectKernelLogs = true;
                 SystemCallArchitectures = "native";
                 RestrictNamespaces = true;
-                MemoryDenyWriteExecute = true;
+                MemoryDenyWriteExecute = false;
                 RestrictAddressFamilies = "AF_UNIX AF_INET AF_INET6";
                 RestrictSUIDSGID = true;
                 NoNewPrivileges = true;
@@ -282,19 +279,14 @@
               rev = photoprism.rev;
               sha256 = photoprism.narHash;
             };
-            libtensorflow-bin = prev.libtensorflow-bin.overrideAttrs (old: {
+            libtensorflow-bin = prev.libtensorflow-bin.overrideAttrs (old: rec {
               # 21.05 does not have libtensorflow-bin 1.x anymore & photoprism isn't compatible with tensorflow 2.x yet
               # https://github.com/photoprism/photoprism/issues/222
+              version = "1.15.0";
               src = fetchurl {
-                url = "https://dl.photoprism.app/tensorflow/linux/libtensorflow-linux-avx2-1.15.2.tar.gz";
-                sha256 = "sha256-zu50uqgT/7DIjLzvJNJ624z+bTXjEljS5Gfq0fK7CjQ=";
+                url = "https://storage.googleapis.com/tensorflow/libtensorflow/libtensorflow-cpu-linux-x86_64-${version}.tar.gz";
+                sha256 = "sha256-3sv9WnCeztNSP1XM+iOTN6h+GrPgAO/aNhfbeeEDTe0=";
               };
-              buildCommand =
-                old.buildCommand
-                + ''
-                  ln -sf $out/lib/libtensorflow.so $out/lib/libtensorflow.so.1
-                  ln -sf $out/lib/libtensorflow_framework.so $out/lib/libtensorflow_framework.so.1
-                '';
             });
           in
             buildGoApplication {
@@ -320,59 +312,33 @@
 
               passthru = rec {
                 inherit libtensorflow-bin;
-                frontend = let
-                  noderanz = callPackage ranz2nix {
-                    nodejs = nodejs-14_x;
-                    sourcePath = src + "/frontend";
-                    packageOverride = name: spec:
-                      if name == "minimist" && spec ? resolved && spec.resolved == "" && spec.version == "1.2.0"
-                      then {
-                        resolved =
-                          "file://"
-                          + (
-                            toString (
-                              fetchurl {
-                                url = "https://registry.npmjs.org/minimist/-/minimist-1.2.0.tgz";
-                                sha256 = "0w7jll4vlqphxgk9qjbdjh3ni18lkrlfaqgsm7p14xl3f7ghn3gc";
-                              }
-                            )
-                          );
-                      }
-                      else {};
-                  };
-                  node_modules = noderanz.patchedBuild;
-                in
-                  stdenv.mkDerivation {
-                    name = "photoprism-frontend";
-                    nativeBuildInputs = [nodejs-14_x];
 
-                    inherit src;
+                frontend = (callPackage inputs.npmlock2nix {}).build {
+                  name = "photoprism-frontend";
+                  src = src + "/frontend";
+                  nodejs = nodejs-14_x;
 
-                    sourceRoot = "source/frontend";
+                  postUnpack = ''
+                    chmod -R +rw .
+                  '';
 
-                    postUnpack = ''
-                      chmod -R +rw .
-                    '';
+                  NODE_ENV = "production";
 
-                    NODE_ENV = "production";
-
-                    buildPhase = ''
-                      export HOME=$(mktemp -d)
-                      ln -sf ${node_modules}/node_modules node_modules
-                      ln -sf ${node_modules.lockFile} package-lock.json
-                      npm run build
-                    '';
-                    installPhase = ''
-                      cp -rv ../assets/static/build $out
-                    '';
-                  };
+                  buildCommands = ["npm run build"];
+                  installPhase = ''
+                    cp -rv ../assets/static/build $out
+                  '';
+                };
 
                 assets = let
                   nasnet = fetchzip {
                     url = "https://dl.photoprism.org/tensorflow/nasnet.zip";
                     sha256 = "09cnr2wpc09xrv1crms3mfcl61rxf4nr5j51ppy4ng6bxg9rq5s1";
                   };
-
+                  facenet = fetchzip {
+                    url = "https://dl.photoprism.org/tensorflow/facenet.zip";
+                    sha256 = "0vyfy7hidlzibm59236ipaymj0mzclnriv9bi86dab1sa627cqpd";
+                  };
                   nsfw = fetchzip {
                     url = "https://dl.photoprism.org/tensorflow/nsfw.zip";
                     sha256 = "0j0r39cgrr0zf2sc1hpr8jh19lr3jxdw9wz6sq3s7kkqay324ab8";
@@ -385,6 +351,7 @@
                     cp -rv ${frontend} $out/static/build
                     ln -s ${nsfw} $out/nsfw
                     ln -s ${nasnet} $out/nasnet
+                    ln -s ${facenet} $out/facenet
                   '';
               };
             }
