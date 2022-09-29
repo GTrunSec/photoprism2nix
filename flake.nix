0e9360f76488ad74ec8c4ba5490268bc9103da5b
GTrunSec <gtrunsec@hardenedlinux.org>
2022-09-29 03:00:32 -0700

    Update

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.05";
     npmlock2nix = {
       url = "github:nix-community/npmlock2nix";
       flake = false;
